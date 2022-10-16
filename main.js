557fde49f432f91e1aa5c7faf2f9b1d0febbf137
Mohinur <hikmatovamohinur2007@gmail.com>
2022-10-16 21:34:42 +0500

    hello

---
+++ main.js
@@ -0,0 +1,41 @@
+// let firstName ='Islom';              
+// let age = 12;         
+// let gender = 'Man';
+// console.log(firstName,age,gender);
+ 
+
+// let name;
+// console.log(name);         
+
+
+// if {
+//     Islom = 12,
+//     console = True
+// }
+
+// else {
+//     Islom = 13,
+//     console = false
+// }
+ 
+// let itcenter ={
+//     name: 'it-center',
+//     computer: 22,
+//     pupils: 25,
+//     air: 3,
+//     teacher: true
+// }
+// console.log(itcenter)
+
+
+
+let chancery ={
+    name: 'chancery',
+    pens: 1000,
+    pensils: 500,
+    notebooks: 500,
+    books: 300, 
+}
+console.log(chancery)
+
+
